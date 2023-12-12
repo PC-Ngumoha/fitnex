ffa7bdfee0dd326d0d042770dc6432724c4a6a83
PC-Ngumoha <pcngumoha@gmail.com>
2023-12-12 22:48:20 +0100

    Update: Started work on the User model: Temporary break

--- backend/fitnex_api/account/models.py
+++ backend/fitnex_api/account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    """ User model definition
+    """
+    pass
