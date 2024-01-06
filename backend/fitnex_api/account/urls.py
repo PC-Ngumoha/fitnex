37f7525934b0ec3208f5e13d375f68892f8ccf67
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2024-01-06 16:46:14 +0100

    added the configuration for sending email - only the file based and console works - smtp still pending

--- backend/fitnex_api/account/urls.py
+++ backend/fitnex_api/account/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     
     # Default reset urls
     path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('account/password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('account/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('account/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
--- backend/fitnex_api/fitnex_api/settings.py
+++ backend/fitnex_api/fitnex_api/settings.py
@@ -168,3 +168,16 @@ CORS_ALLOW_HEADERS = (
 CORS_ALLOW_METHODS = ('GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+# Email Configuration
+# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
